679550fee5370a774350f763041c29775600470c
Jong <freesetting@naver.com>
2018-11-21 14:42:38 +0900

    stom3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into user_login values ('han', '1234','한범석');
-insert into user_login values ('admin', '5678','관리자');
+insert into user_login values ('admin', '1234','관리자');
+insert into user_login values ('고객이다', '1234','고객');
